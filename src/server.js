25ab6a1793fdb938b9ab0cc35419c5d7b76d352e
Thiago Antonio <taoalu@gmail.com>
2021-03-24 13:47:19 -0300

    Conexão a base de dados

--- src/server.js
+++ src/server.js
@@ -7,22 +7,22 @@ const app = express();
 const MONGO_ATLAS_URI = 'mongodb+srv://taoalu:thi15ago87@cluster0.nzpzi.mongodb.net/projetoBD2?retryWrites=true&w=majority';
 
 // connect to mongodb
-//mongoose.connect(MONGO_ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGO_ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.on('connected',()=>{
-    console.log('Connected to Database!' + mongoose.mongo.Admin);
+mongoose.connection.on('connected', () => {
+    console.log(mongoose.connection.readyState == 1 ? "Database Connected" : "Database Disconnected");
 });
 
-var connection = mongoose.createConnection(
-    MONGO_ATLAS_URI,{useNewUrlParser: true, useUnifiedTopology: true});
-connection.on('open', function() {
-    // connection established
-    new Admin(connection.db).listDatabases(function(err, result) {
-        console.log('listDatabases succeeded');
-        // database list stored in result.databases
-        var allDatabases = result.databases;    
-    });
-});
+// var connection = mongoose.createConnection(
+//     MONGO_ATLAS_URI,{useNewUrlParser: true, useUnifiedTopology: true});
+// connection.on('open', function() {
+//     // connection established
+//     new Admin(connection.db).listDatabases(function(err, result) {
+//         console.log('listDatabases succeeded');
+//         // database list stored in result.databases
+//         var allDatabases = result.databases;    
+//     });
+// });
 
 app.listen(process.env.PORT || 3000, function () {
     console.log(
