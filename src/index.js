1623b51b7569f691c9294ddff11bdb3238b0f9ee
taoalu <taoalu@gmail.com>
2021-03-23 22:12:34 -0300

    Criacao do configuracao inicial do projeto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Projeto DB2');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req,res)=>{
+res.send('OK');
+});
+
+app.listen(3000);
