3454545232f1b116fa83d0f7f34a84cda2263840
Thiago Antonio <taoalu@gmail.com>
2021-03-24 11:10:44 -0300

    conexão com o banco funcional

--- src/routes/api.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/projetoDB');
-mongoose.Promise = global.Promise;
-
--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'), Admin = mongoose.mongo.Admin;
 
 // set up our express app
 const app = express();
 
+const MONGO_ATLAS_URI = 'mongodb+srv://taoalu:thi15ago87@cluster0.nzpzi.mongodb.net/projetoBD2?retryWrites=true&w=majority';
+
 // connect to mongodb
-mongoose.connect('mongodb+srv://taoalu:thi15ago87@cluster0.nzpzi.mongodb.net/projetoBD2?retryWrites=true&w=majority');
-mongoose.Promise = global.Promise;
+//mongoose.connect(MONGO_ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to Database!' + mongoose.mongo.Admin);
+});
+
+var connection = mongoose.createConnection(
+    MONGO_ATLAS_URI,{useNewUrlParser: true, useUnifiedTopology: true});
+connection.on('open', function() {
+    // connection established
+    new Admin(connection.db).listDatabases(function(err, result) {
+        console.log('listDatabases succeeded');
+        // database list stored in result.databases
+        var allDatabases = result.databases;    
+    });
+});
 
 app.listen(process.env.PORT || 3000, function () {
     console.log(
